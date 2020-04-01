943668caed9b7c9eb99de6aad9866c88ac6e26f8
mokrz <mokrz@github.com>
2020-04-01 16:22:10 -0700

    Add some critical functionality

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Hello world!")
+	return
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	serveErr := http.ListenAndServe(":8080", nil)
+
+	if serveErr != nil {
+		fmt.Printf("failed to ListenAndServe with error: " + serveErr.Error())
+	}
+
+	return
+}
