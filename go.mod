521612a8e0378eaf8c6c56666fc226d3f37b7c28
mokrz <mokrz@github.com>
2020-06-21 13:30:35 -0700

    libp2p fun

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/mokrz/testsvc
+
+go 1.14
+
+require (
+	github.com/libp2p/go-libp2p v0.10.0
+	github.com/libp2p/go-libp2p-core v0.6.0
+	github.com/libp2p/go-libp2p-kad-dht v0.8.2
+	github.com/libp2p/go-libp2p-pubsub v0.3.2
+	github.com/multiformats/go-multiaddr v0.2.2
+)
--- main.go
+++ main.go
@@ -1,25 +1,197 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"flag"
 	"fmt"
-	"net/http"
+	"math/rand"
+	"time"
+
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/peer"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/libp2p/go-libp2p/p2p/discovery"
 )
 
-func TestMe(n int) int {
-	return n * 2
+type discoveryNotifee struct {
+	node host.Host
 }
 
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(w, "Hello world!")
+func (n *discoveryNotifee) HandlePeerFound(pi peer.AddrInfo) {
+	fmt.Printf("[+] Discovered peer %s\n", pi.ID.Pretty())
+	err := n.node.Connect(context.Background(), pi)
+
+	if err != nil {
+		fmt.Printf("error connecting to peer %s: %s\n", pi.ID.Pretty(), err)
+	}
+}
+
+func handleStream(stream network.Stream) {
 	return
 }
 
-func main() {
-	http.HandleFunc("/hello", helloHandler)
-	serveErr := http.ListenAndServe(":8080", nil)
+func configureDiscovery(ctx context.Context, node host.Host) error {
+	disc, err := discovery.NewMdnsService(ctx, node, time.Hour, "testing")
 
-	if serveErr != nil {
-		fmt.Printf("failed to ListenAndServe with error: " + serveErr.Error())
+	if err != nil {
+		return err
+	}
+
+	n := discoveryNotifee{node: node}
+	disc.RegisterNotifee(&n)
+
+	return nil
+}
+
+type part struct {
+	Hash string `json:"hash"`
+	Path string `json:"Path"`
+}
+
+type dfile struct {
+	subscription *pubsub.Subscription
+	topic        *pubsub.Topic
+	self         peer.ID
+	parts        chan *part
+}
+
+func (df *dfile) readLoop() {
+	for {
+		var (
+			msg *pubsub.Message
+			p   part
+			err error
+		)
+
+		if msg, err = df.subscription.Next(context.Background()); err != nil {
+			close(df.parts)
+			return
+		}
+
+		if err = json.NewDecoder(bytes.NewReader(msg.Data)).Decode(&p); err != nil {
+			fmt.Printf("failed to decode message data: %s\n", err.Error())
+			close(df.parts)
+			return
+		}
+
+		df.parts <- &p
+	}
+}
+
+func (df *dfile) writeLoop() {
+	rand.Seed(time.Now().UnixNano())
+
+	randString := func(n int) string {
+		letterRunes := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+		b := make([]rune, n)
+
+		for i := range b {
+			b[i] = letterRunes[rand.Intn(len(letterRunes))]
+		}
+
+		return string(b)
+	}
+
+	for {
+		r := randString(8)
+		b, err := json.Marshal(&part{
+			Hash: r,
+			Path: "/tmp/test",
+		})
+
+		if err != nil {
+			fmt.Printf("failed to marshal message data: %s\n", err.Error())
+			return
+		}
+
+		if err = df.topic.Publish(context.Background(), b); err != nil {
+			fmt.Printf("failed to publish: %s\n", err.Error())
+			return
+		}
+
+		fmt.Printf("[+] Published: %s\n", string(b))
+		time.Sleep(time.Duration(rand.Intn(7)) * time.Second)
+	}
+}
+
+func main() {
+	var (
+		ctx                                      context.Context
+		opts                                     []libp2p.Option
+		node                                     host.Host
+		ps                                       *pubsub.PubSub
+		topic                                    *pubsub.Topic
+		sub                                      *pubsub.Subscription
+		nodeErr, psErr, discErr, joinErr, subErr error
+	)
+	ctx = context.Context(context.Background())
+	opts = []libp2p.Option{
+		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"),
+		libp2p.DisableRelay(),
+		libp2p.Ping(false),
+	}
+
+	flag.Parse()
+
+	if node, nodeErr = libp2p.New(ctx, opts...); nodeErr != nil {
+		fmt.Printf("failed to create libp2p node: %s\n", nodeErr.Error())
+		return
+	}
+
+	if ps, psErr = pubsub.NewGossipSub(ctx, node); psErr != nil {
+		fmt.Printf("failed to create gossip sub service: %s\n", psErr.Error())
+		return
+	}
+
+	if discErr = configureDiscovery(ctx, node); discErr != nil {
+		fmt.Printf("failed to configure discovery: %s\n", discErr.Error())
+		return
+	}
+
+	if topic, joinErr = ps.Join("testing"); joinErr != nil {
+		fmt.Printf("failed to join testing topic: %s\n", joinErr.Error())
+		return
+	}
+
+	if sub, subErr = topic.Subscribe(); subErr != nil {
+		fmt.Printf("failed to subscibe to testing topic: %s\n", subErr.Error())
+		return
+	}
+
+	fmt.Printf("[+] Subscribed to topic\n")
+
+	df := &dfile{
+		subscription: sub,
+		self:         node.ID(),
+		topic:        topic,
+		parts:        make(chan *part, 128),
+	}
+
+	go df.writeLoop()
+
+	for {
+		var (
+			msg *pubsub.Message
+			p   part
+			err error
+		)
+
+		if msg, err = df.subscription.Next(context.Background()); err != nil {
+			close(df.parts)
+			break
+		}
+
+		if err = json.NewDecoder(bytes.NewReader(msg.Data)).Decode(&p); err != nil {
+			fmt.Printf("failed to decode message data: %s\n", err.Error())
+			close(df.parts)
+			break
+		}
+
+		fmt.Printf("Got: %s from %s\n", string(msg.Data), msg.GetFrom())
 	}
 
 	return
